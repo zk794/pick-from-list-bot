bbf95006f16040f061ee7a07fb2a6999e42758c0
ZKatok <zkatok@sageridge.org>
2021-06-08 18:16:52 -0500

    bot says hello

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!listbot'):
+        await message.channel.send('Hello!')
+
+client.run(os.getenv('TOKEN'))
+TOKEN=[ODUxOTU2MjgxNTk3ODg2NDc0.YL_0Hg.gMTZ5EjwPbb7XvDORWmcvmoonnw]
